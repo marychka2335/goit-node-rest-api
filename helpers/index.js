cd1078f89a1e1b1ebe042557e24940678641bd0d
marychka2335 <marychka2335@icloud.com>
2024-05-05 22:17:19 +0300

    hw commit

--- helpers/index.js
+++
@@ -1,9 +0,0 @@
-const HttpError = require('./HttpError');
-const handleMongooseError = require('./handleMongooseError');
-const resizeImage = require('./resizeImage')
-
-module.exports = {
-    HttpError,
-    handleMongooseError,
-    resizeImage
-}
