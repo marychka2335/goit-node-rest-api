cd35003ed3ab665e31fca03f4a7fc6228d377542
marychka2335 <marychka2335@icloud.com>
2024-06-03 00:39:17 +0300

    mistakes fixed

--- middlewares/isEmptyField.js
+++ middlewares/isEmptyField.js
@@ -1,6 +1,6 @@
 import HttpError from "../helpers/HttpError.js";
 
-const isEmptyField= (req, res, next) => {
+const isEmptyField= (req, _, next) => {
   const { length } = Object.keys(req.body);
   if (!length) {
     return next(HttpError(400, "Body must have at least one field"));
