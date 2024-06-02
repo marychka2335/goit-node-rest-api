12c7ea923353474c245c82ea66b66ffad9727907
marychka2335 <marychka2335@icloud.com>
2024-06-03 02:21:43 +0300

    mistakes fixed

---
+++ middlewares/index.js
@@ -0,0 +1,21 @@
+const { HttpError } = require('./HttpError');
+const {handleMongooseError} = require('./handleMongooseError');
+const resizeImage = require('./resizeImage')
+
+const {validateBody} = (schema) => {
+    const func = (req, _, next) => {
+        const { error } = schema.validate(req.body);
+        if (error) {
+            next(HttpError(400, error.message));
+        }
+        next();
+    };
+    return func;
+};
+
+module.exports = {
+    HttpError,
+    validateBody,
+    handleMongooseError,
+    resizeImage
+}
---
+++ schemas/index.js
@@ -0,0 +1,13 @@
+const addSchema = require('./addSchema');
+const updateFavoriteSchema = require('./updateFavoriteSchema');
+const userSchema = require('./userSchema');
+const updateUserSchema = require('./updateUserSchema');
+const emailSchema = require('./emailSchema')
+
+module.exports = {
+    addSchema,
+    updateFavoriteSchema,
+    userSchema,
+    updateUserSchema,
+    emailSchema
+}
