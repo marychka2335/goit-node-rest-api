b61aff1b85978d28868748d6c80e5bf7d1194a82
marychka2335 <marychka2335@icloud.com>
2024-06-03 02:21:24 +0300

    mistakes fixed

--- schemas/emailSchema.js
+++
@@ -1,9 +0,0 @@
-const Joi = require('joi');
-
-const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-const emailSchema = Joi.object({
-    email: Joi.string().pattern(emailRegexp).required(),
-})
-
-module.exports = emailSchema
