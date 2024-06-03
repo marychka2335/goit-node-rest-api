9e40c42e583256ccf410525f909f312a9b530d9f
marychka2335 <marychka2335@icloud.com>
2024-06-03 03:19:22 +0300

    mistakes fixed

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const controllers = require('../../controllers');
-const { isEmptyField } = reqire('../../middlewares/isEmptyField.js');
 const { validateBody } = require('../../helpers/index.js');
 const { authenticate, upload } = require('../../middlewares');
 const { validateBody } = require('../../helpers/index.js');
@@ -8,11 +7,11 @@ const { userSchema } = require('../../schemas/userSchema');
 const { updateUserSchema } = require('../../schemas/updateUserSchema')
 const router = express.Router();
 
-router.post('/register', isEmptyField, validateBody(userSchema), controllers.register);
-router.post('/login', isEmptyField, validateBody(userSchema),controllers.login);
+router.post('/register',  validateBody(userSchema), controllers.register);
+router.post('/login',  validateBody(userSchema),controllers.login);
 router.get('/current', authenticate, controllers.getCurrent);
 router.post('/logout', authenticate, controllers.logout);
-router.patch('/', authenticate, isEmptyField, validateBody(updateUserSchema), controllers.updateUserSub),
+router.patch('/', authenticate, validateBody(updateUserSchema), controllers.updateUserSub),
 router.patch('/avatars', authenticate, upload.single('avatar'), controllers.updateAvatar)
 
 module.exports = router
