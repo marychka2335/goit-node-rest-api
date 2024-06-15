c9ddb9b5d0ad33ca9f7801a63f2d679f0ccc18ba
marychka2335 <marychka2335@icloud.com>
2024-06-15 03:42:47 +0300

    port number changed

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use((err, req, res, next) => {
   res.status(status).json({ message });
 });
 
-app.listen(3016, () => {
-  console.log("Server is running. Use our API on port: 3016");
+app.listen(3000, () => {
+  console.log("Server is running. Use our API on port: 3000");
 });
 
