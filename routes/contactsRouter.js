e4a8f19b775f98582f7aecef44938b2e805b0c70
marychka2335 <marychka2335@icloud.com>
2024-06-27 18:57:27 +0300

    finish hw task

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -1,22 +1,47 @@
 import express from "express";
+import contactsControllers from "../controllers/contactsControllers.js";
+import isEmptyBody from "../middlewares/isEmptyBody.js";
+import isValidId from "../middlewares/isValidId.js";
+import validateBody from "../helpers/validateBody.js";
 import {
-  getAllContacts,
-  getOneContact,
-  deleteContact,
-  createContact,
-  updateContact,
-} from "../controllers/contactsControllers.js";
+  createContactSchema,
+  updateContactSchema,
+  updateContactStatusSchema,
+} from "../schemas/contactsSchemas.js";
 
 const contactsRouter = express.Router();
 
-contactsRouter.get("/", getAllContacts);
+contactsRouter.get("/", contactsControllers.getAllContacts);
 
-contactsRouter.get("/:id", getOneContact);
+contactsRouter.get("/:contactId", isValidId, contactsControllers.getOneContact);
 
-contactsRouter.delete("/:id", deleteContact);
+contactsRouter.delete(
+  "/:contactId",
+  isValidId,
+  contactsControllers.deleteContact
+);
 
-contactsRouter.post("/", createContact);
+contactsRouter.post(
+  "/",
+  isEmptyBody,
+  validateBody(createContactSchema),
+  contactsControllers.createContact
+);
 
-contactsRouter.put("/:id", updateContact);
+contactsRouter.put(
+  "/:contactId",
+  isValidId,
+  isEmptyBody,
+  validateBody(updateContactSchema),
+  contactsControllers.updateContact
+);
+
+contactsRouter.patch(
+  "/:contactId/favorite",
+  isValidId,
+  isEmptyBody,
+  validateBody(updateContactStatusSchema),
+  contactsControllers.updateStatusContact
+);
 
 export default contactsRouter;
