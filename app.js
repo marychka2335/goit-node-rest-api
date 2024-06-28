69f731d303283a05eff15e05116ca614dc185cd5
marychka2335 <marychka2335@icloud.com>
2024-06-28 03:23:36 +0300

    mistakes fixed

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import 'dotenv/config';
 import morgan from 'morgan';
 import cors from 'cors';
-import { connect } from 'mongoose';
-// import 'colors';
+import mongoose from 'mongoose';
 import express from 'express';
+
 import contactsRouter from './routes/contactsRouter.js';
-const { DB_HOST, PORT } = process.env;
+
+const { DB_HOST, PORT = 3001 } = process.env;
 const app = express();
 
 app.use(morgan('tiny'));
@@ -22,58 +23,25 @@ app.use((err, req, res, next) => {
   const { status = 500, message = 'Server error' } = err;
   res.status(status).json({ message });
 });
-try {
-	await connect(DB_HOST)
-	console.log('Database connection successful');
-	app.listen(PORT, () => {
-		console.log(
-			`Server running. Use our API on port: ${PORT}`);
-	});
-} catch (err) {
-	console.log(  'Could not connect to the mongodb database because of error',
-      err.message);
-	process.exit(1);
-}
 
 
-// import mongoose from 'mongoose';
-// import 'dotenv/config';
+mongoose
+  .connect('mongodb+srv://marychka2335:HiVidahSW1MKDm4S@cluster0.a95orri.mongodb.net/db-contacts', {
+    usenewurlparser: true,
+    useunifiedtopology: true,
+  } )
+  .then(() => {
+    console.log('Database connection successful');
+    app.listen(PORT, () => {
+      console.log(`Server is running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch(error => {
+    console.log(
+      'Could not connect to the mongodb database because of error',
+      error.message
+    );
+    process.exit(1);
+  });
 
-// import contactsRouter from './routes/contactsRouter.js';
-
-// const { DB_HOST, PORT = 3000 } = process.env;
-
-// const app = express();
-
-// app.use(morgan('tiny'));
-// app.use(cors());
-// app.use(express.json());
-
-// app.use('/api/contacts', contactsRouter);
-
-// app.use((_, res) => {
-//   res.status(404).json({ message: 'Route not found' });
-// });
-
-// app.use((err, req, res, next) => {
-//   const { status = 500, message = 'Server error' } = err;
-//   res.status(status).json({ message });
-// });
-
-// mongoose
-//   .connect(DB_HOST)
-//   .then(() => {
-//     console.log('Database connection successful');
-//     app.listen(PORT, () => {
-//       console.log(`Server is running. Use our API on port: ${PORT}`);
-//     });
-//   })
-//   .catch(error => {
-//     console.log(
-//       'Could not connect to the mongodb database because of error',
-//       error.message
-//     );
-//     process.exit(1);
-//   });
-
-// mongoose.set('debug', true);
+mongoose.set('debug', true);
