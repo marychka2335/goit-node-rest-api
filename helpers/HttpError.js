a8021c79cb7d3a3891f48a5ce518675315586abf
marychka2335 <marychka2335@icloud.com>
2024-07-12 22:40:44 +0300

    mistakes fixed

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -1,6 +1,6 @@
 const messageList = {
     400: "Bad Request",
-    401: "Not Authorized",
+    401: "Unauthorized",
     403: "Forbidden",
     404: "Not Found",
     409: "Conflict",
@@ -9,7 +9,7 @@ const messageList = {
 const HttpError = (status, message = messageList[status]) => {
     const error = new Error(message);
     error.status = status;
-    return error
+    return error;
 }
 
-module.exports = HttpError
+export default HttpError;
